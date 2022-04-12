3f71a7d0dbfc3b99181b551ddad9f7a5c78ed670
kiwifruit-guo <786023299@qq.com>
2022-04-12 23:45:42 +0800

    修复了无法添加浏览记录的bug

--- detail_page.py
+++ detail_page.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, jsonify, request
 from models import House, User, Tuijian
 from utils.connect_to_database import query_data
+from settings import db
 from sqlalchemy import func
 
 # # from utils.regression_data import linear_model_main
@@ -16,20 +17,48 @@ detail_page = Blueprint('detail_page', __name__)
 3. 使用render_template进行模板的渲染
 4. 展示一次页面自动使得浏览量加一
 """
-
+#添加浏览记录
+# 登陆状态下：cookie-name-浏览记录：
+#     已经存在浏览记录：
+#         hid在浏览记录中：
+#             pass
+#         else:
+#             添加hid，返回房源详情
+#     else:
+#         插入hid，返回房源详情
 
 @detail_page.route('/house/<int:hid>')
 def detail(hid):
     house = House.query.get(hid)
     liulangliang = house.liulanliang
     id = str(house.id)
-    sql_list = ['use beijing_house_data;', f'update soufang set liulanliang=liulanliang+1 where id={id}']
-    for sql in sql_list:
-        result = query_data(sql)
-    return render_template('detail_page.html', house=house)
+    # sql_list = ['use beijing_house_data;', f'update soufang set liulanliang=liulanliang+1 where id={id}']
+    # for sql in sql_list:
+    #     result = query_data(sql)
+    house.liulanliang+=1
+    db.session.commit()
+    sheshi_str = house.sheshi  # 床-宽带-洗衣机-空调-热水器-暖气
+    sheshi_list = sheshi_str.split('-')
 
-    # sheshi_str = house.sheshi  # 床-宽带-洗衣机-空调-热水器-暖气
-    # sheshi_list = sheshi_str.split('-')
+    name=request.cookies.get('name')
+    if name:
+        user=User.query.filter(User.name==name).first()
+        seen_id_str=user.seen_id
+        # print(seen_id_str)
+        if seen_id_str:
+            seen_id_list=seen_id_str.split(',')
+            set_id=set([int(i) for i in seen_id_list])
+            if hid in set_id:
+                pass
+            else:
+                user.seen_id=seen_id_str+str(hid)
+                db.session.commit()
+
+        else:
+            user.seen_id=str(hid)
+            db.session.commit()
+    return render_template('detail_page.html', house=house,sheshi=sheshi_list)
+
 
 
 # 可以添加价格走势，户型占比，房源数量，户型价格走势，推荐房源
